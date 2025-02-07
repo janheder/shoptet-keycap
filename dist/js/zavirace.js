8f2f32335af1577727c21f35dc9cc3c4a02eb278
Jan Heder <info@janheder.cz>
2025-02-07 10:04:13 +0100

    Update zavirace.js

--- dist/js/zavirace.js
+++ dist/js/zavirace.js
@@ -1,8 +1,8 @@
 if ($("#checkoutSidebar .cart-item-gift").length){
     const loadPicImages = (html) => {
         let nodes = new DOMParser().parseFromString(html, 'text/html');
-        let body = nodes.querySelectorAll('.free-gifts-img');
-        let num = nodes.querySelectorAll('.free-gifts-img').length;
+        let body = nodes.querySelectorAll('#vyberte-jiny-darek li.active .free-gifts-img');
+        let num = nodes.querySelectorAll('#vyberte-jiny-darek li.active .free-gifts-img').length;
         for(let i = 0; i < num; i++) {
             document.querySelector('#checkoutSidebar .cart-item-gift:nth-child('+ (i+1) +')').prepend(body[i]);
         }
