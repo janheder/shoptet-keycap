7282d39464f71a805ee8cbb103ddee4c12ed881e
Jan Heder <info@janheder.cz>
2025-05-02 13:17:20 +0200

    .

--- dist/js/olpran.js
+++ dist/js/olpran.js
@@ -42,3 +42,109 @@ if ($(".in-index").length) {
             console.error("Error fetching /blog:", error);
         });
 }
+
+
+
+// =============================================================================
+// ADVANCED ORCDER SUMMARY
+// =============================================================================
+
+
+if ($(".type-detail").length){
+
+    $("body").append('<div class="advancedModal"><div class="advancedModal__inner"><h2 class="advancedModal__title">Zboží bylo přidáno do košíku</h2><div class="advancedModal__content"></div><div class="advancedModal__buttons"><a href="#" class="btn btn-ghost" id="advancedModalBack">Zpět do obchodu</a><a href="/kosik" class="btn">Přejít do košíku</a></div></div></div>');
+
+    if ($(".products-related").length){
+        var related = $(".products-related").html();
+        $(".advancedModal__inner").append('<div id="productsRelated"><h3 class="advancedModal__relatedTitle">Související produkty</h3><div class="products-block">' + related + '</div></div>');
+    }
+    
+    function advanceOrderCustom() {
+    
+        $("body").addClass("--advancedModal");
+        $(".advancedModal__content").html("");
+    
+        var img = $(".p-detail-inner .p-image-wrapper a").html();
+        var name = $(".p-detail-inner .p-detail-inner-header h1").html();
+        if($(".p-detail-inner .parameter-dependent").length){
+            var stock = $(".p-detail-inner .availability-value .parameter-dependent:not(.noDisplay) span").html();
+        }else{
+            var stock = $(".p-detail-inner .availability-value").html();
+        }
+        var amount = parseFloat($(".p-detail-inner .add-to-cart .amount").val());
+    
+        var priceSingle = $(".p-detail-inner .p-final-price-wrapper .price-final-holder:not(.noDisplay)").html();
+        var priceTotal = parseFloat(priceSingle.replace(/ /g, ''))*amount;
+    
+        $(".advancedModal__content").prepend('<div class="advancedProduct">' +
+        '<div class="advancedProduct-img">' + img + '</div>' +
+        '<div class="advancedProduct-content">' +
+        '<h4 class="advancedProduct-name">' + name + '</h4>' +
+        '<div class="advancedProduct-stock">Dostupnost<span>' + stock + '</span></div>' +
+        '<div class="advancedProduct-amount">Počet kusů<span>' + amount + 'x</span></div>' +
+        '<div class="advancedProduct-priceTotal">Celková cena<span>' + priceTotal + ' Kč</span></div>' +
+        '</div></div>');
+        
+    }
+    
+    /* call functions after order modal loaded */
+    document.addEventListener('ShoptetCartAddCartItem', function () {
+        advanceOrderCustom();
+    },{
+        passive: true
+    });
+
+    $('.advancedModal').on('click',function(e){
+        if (e.target !== this)
+        return;
+        $("body").removeClass("--advancedModal");
+    });
+
+    $('#advancedModalBack').on('click',function(){
+        $("body").removeClass("--advancedModal");
+    });
+}
+
+
+if ($(".type-index, .type-category").length){
+
+    $("body").append('<div class="advancedModal --product"><div class="advancedModal__inner"><h2 class="advancedModal__title">Zboží bylo přidáno do košíku</h2><div class="advancedModal__content"></div><div class="advancedModal__buttons"><a href="#" class="btn btn-ghost">Zpět do obchodu</a><a href="/kosik" class="btn">Přejít do košíku</a></div></div></div>');
+
+    
+    /* call functions after order modal loaded */
+    $(".add-to-cart-button").on('click',function(){
+
+        var img = $(this).closest(".p").find(".image").html();
+        var name = $(this).closest(".p").find(".name").html();
+        var stock = $(this).closest(".p").find(".availability").html();
+
+        var amount = parseFloat($(this).closest(".p").find("input[name='amount']").val());
+
+        var priceSingle = $(this).closest(".p").find(".price-final strong").html();
+        var priceTotal = parseFloat(priceSingle.replace(/ /g, ''))*amount;
+
+        document.addEventListener('ShoptetCartAddCartItem', function () {
+            $("body").addClass("--advancedModal");
+            $(".advancedModal__content").html("");
+        
+            $(".advancedModal__content").prepend('<div class="advancedProduct">' +
+            '<div class="advancedProduct-img">' + img + '</div>' +
+            '<div class="advancedProduct-content">' +
+            '<div class="advancedProduct-name">' + name + '</div>' +
+            '<div class="advancedProduct-stock">Dostupnost<span>' + stock + '</span></div>' +
+            '<div class="advancedProduct-amount">Počet kusů<span>' + amount + 'x</span></div>' +
+            '<div class="advancedProduct-priceTotal">Celková cena<span>' + priceTotal + ' Kč</span></div>' +
+            '</div></div>');
+        },{
+            passive: true
+        });
+    });
+
+
+    $('.advancedModal').on('click',function(e){
+        if (e.target !== this)
+        return;
+        $("body").removeClass("--advancedModal");
+    });
+}
+
